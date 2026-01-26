f81ff201c567474811c44543d2d57f9b8d5470d1
Adrian <adrianrosten@gmail.com>
2026-01-26 17:31:25 +0100

    Fix backend test

--- backend/src/test/java/com/fisk/dayboardapi/DayboardApiApplicationTests.java
+++ backend/src/test/java/com/fisk/dayboardapi/DayboardApiApplicationTests.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DayboardApiApplicationTests {
 
+    static {
+        System.setProperty("DAYBOARD_APP_PATH", "");
+    }
+
     @Test
     void contextLoads() {
     }
