f7b9bb46592f265617a11ff27e7155b252cc82b8
Adrian <adrianrosten@gmail.com>
2026-02-07 14:18:23 +0100

    Update backend port to 1292

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,7 +1,7 @@
 
 export const RETRY_REQUEST_HEADER = "X-Retry-Request"
 
-export const BASE_URL = "http://localhost:8080/";
+export const BASE_URL = "http://localhost:1292/";
 export const EVENTS_ENDPOINT = BASE_URL + "api/events";
 export const DASHBOARD_ENDPOINT = BASE_URL + "api/dashboard";
 
